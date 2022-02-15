86abd190589b03288de5fe8b5dcabf747a286709
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-02-15 19:44:28 +0900

    正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,13 @@ class User < ApplicationRecord
   validates :last_name_kana,  presence: true
   validates :birthday,        presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password,        with: PASSWORD_REGEX
+  validates_format_of :first_name,      with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  validates_format_of :first_name_kana, with: /\A[ァ-ヶ]+\z/
+  validates_format_of :last_name,       with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  validates_format_of :last_name_kana,  with: /\A[ァ-ヶ]+\z/
+
   # has_many :tweets, :orders 
   # アソシエーションはテーブル作成時に記述する
 
