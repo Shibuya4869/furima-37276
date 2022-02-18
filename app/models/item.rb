f4aac516f9de659f5def885c95015c4fe0c724d2
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-02-18 15:32:58 +0900

    マイグレーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :name,        presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :schedule_id, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
+  # has_one :order
+  # アソシエーションはテーブル作成時に記述する
 end
