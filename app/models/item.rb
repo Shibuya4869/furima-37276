000a88951b95fedf4a8f20f12c448b9040b111e9
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-02-18 15:37:01 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-  validates :name,        presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
+  validates :name,            presence: true
+  validates :description,     presence: true
+  validates :category_id,     presence: true
+  validates :condition_id,    presence: true
   validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :schedule_id, presence: true
-  validates :price, presence: true
+  validates :prefecture_id,   presence: true
+  validates :schedule_id,     presence: true
+  validates :price,           presence: true
 
   belongs_to :user
   # has_one :order
