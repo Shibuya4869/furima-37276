f25b777dd0511917072c57a9b6caa3d8b1722762
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-02-19 10:48:56 +0900

    ActiveHashのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   validates :name,            presence: true
   validates :description,     presence: true
-  validates :category_id,     presence: true
-  validates :condition_id,    presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id,   presence: true
-  validates :schedule_id,     presence: true
-  validates :price,           presence: true
+  validates :category_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :schedule_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,           presence: true, numericality: { only_integer: true, greater_than: 299 , less_than: 10000000, message: "300円以上1千万円未満で入力してください" }
 
   belongs_to :user
   # has_one :order ←テーブル作成時に記述する
