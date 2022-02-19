9ee268aca06355298df83871f69a15c3ad112821
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-02-19 15:18:21 +0900

    コントローラの実装とエラー表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,12 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :schedule_id,     presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,           presence: true, numericality: { only_integer: true, greater_than: 299 , less_than: 10000000, message: "300円以上1千万円未満で入力してください" }
+  validates :price,           presence: true, numericality: { only_integer: true, greater_than: 299 , less_than: 10000000, message: "is out of setting range" }
+  validates :image,           presence: true
 
   belongs_to :user
   # has_one :order ←テーブル作成時に記述する
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :schedule_id
+  belongs_to :category, :condition, :shipping_fee, :prefecture, :schedule
   has_one_attached :image
 end
