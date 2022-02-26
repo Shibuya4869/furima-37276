0b599281c756995571d893469c835af0df6f2b0c
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-02-26 17:59:39 +0900

    ストロングパラメータとバリデーション

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,17 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    praams.require(:address).permit(:order_id, :post_code, :prefecture_id, :city, :address, :building, :phone)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
+  validates :post_code,     presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city,          presence: true
+  validates :address,       presence: true
+  validates :phone,         presence: true, numericality: { only_integer: true, in: 10..11}
+
   belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
