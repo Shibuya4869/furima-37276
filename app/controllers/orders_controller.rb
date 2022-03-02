8712463e7be56e0ff98414db4eb700eb61b26dfa
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-03-02 19:27:13 +0900

    クレジット購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,25 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      Payjp.api_key = "sk_test_34be3d42c8d7d3bcacde5d57"
+      Payjp::Charge.create(
+        amount: order_params[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:item_id).merge(user_id: current_user.id)
+    params.require(:order).permit(:item_id).merge(user_id: current_user.id, token: params[:token])
   end
 
   def address_params
