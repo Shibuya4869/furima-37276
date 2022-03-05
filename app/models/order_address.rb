209168633f632e4509f4b7f3e085b1e5b609691f
Shibuya4869 <96322971+Shibuya4869@users.noreply.github.com>
2022-03-05 10:54:58 +0900

    order_addressモデルの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,17 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :token :user_id, :item_id, :order_id, :post_code, :prefecture_id, :city, :address, :building, :phone
+
+  validates :token,         presence: true
+  validates :post_code,     presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city,          presence: true
+  validates :address,       presence: true
+  validates :phone,         presence: true, numericality: { only_integer: true, in: 10..11}
+
+  def save
+    Order.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order_id, post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone: phone)
+  end
+
+end
